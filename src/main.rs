8faf119a0e49d59d9ea76a58179c25c218c4e530
vcarvalho0 <vitor1.santos00@gmail.com>
2023-03-19 22:16:05 -0300

    feat: Add prefix and command

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,32 @@
 use std::env;
 
-use serenity::async_trait;
-use serenity::model::channel::Message;
-use serenity::model::gateway::Ready;
-use serenity::prelude::*;
+use serenity::{
+    async_trait,
+    framework::{standard::{
+        CommandOptions, CommandGroup, CommandResult,
+        macros::{command, group}
+    }, StandardFramework},
+    model::{channel::Message, gateway::Ready},
+    prelude::*
+};
+
+#[group]
+#[commands(hello)]
+struct General;
+
+#[command]
+async fn hello(ctx: &Context, msg: &Message) -> CommandResult {
+    if let Err(why) = msg.channel_id.say(&ctx.http, "Hello World!").await {
+        println!("Error sending message: {:?}", why);
+    }
+
+    Ok(())
+}
 
 struct Handler;
 
 #[async_trait]
 impl EventHandler for Handler {
-    async fn message(&self, ctx: Context, msg: Message) {
-        if msg.content == "!hello" {
-            if let Err(why) = msg.channel_id.say(&ctx.http, "Hello World!").await {
-                println!("Error sending message: {:?}", why);
-            }
-        }
-    }
-
     async fn ready(&self, _: Context, ready: Ready) {
         println!("{} is connected!", ready.user.name);
     }
@@ -24,12 +34,23 @@ impl EventHandler for Handler {
 
 #[tokio::main]
 async fn main() {
-    let token = env::var("DISCORD_TOKEN").expect("Expected a token in the environment");
+    let token = env::var("DISCORD_TOKEN")
+        .expect("Expected a token in the environment");
+
+    let framework = StandardFramework::new()
+    .configure(|config_obj| config_obj.prefix("."));
+
     let intents = GatewayIntents::GUILD_MESSAGES
         | GatewayIntents::DIRECT_MESSAGES
         | GatewayIntents::MESSAGE_CONTENT;
+
     let mut client =
-        Client::builder(&token, intents).event_handler(Handler).await.expect("Err creating client");
+        Client::builder(&token, intents)
+            .event_handler(Handler)
+            .framework(framework)
+            .await
+            .expect("Err creating client");
+
     if let Err(why) = client.start().await {
         println!("Client error: {:?}", why);
     }
